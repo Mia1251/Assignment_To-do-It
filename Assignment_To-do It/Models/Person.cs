60da6c9b9fda54559b4129d332ef690625c7d293
Maria Svensson <svanteomax@gmail.com>
2022-09-21 15:49:26 +0200

    220921 updated file

---
+++ Assignment_To-do It/Models/Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_To_do_It.Models
+{
+    public class Person
+    {
+        private readonly int Id;
+        private string FirstName;
+        private string LastName;
+        public Person(int id, string firstName, string lastName)
+        {
+            this.Id = id;
+            this.FirstName = firstName;  
+            this.LastName = lastName;    
+        }
+        public int id { 
+            get { return Id; }
+
+        }
+        public string firstName
+        {
+            get { return FirstName; }
+            set {
+                if (string.IsNullOrEmpty(firstName))
+                {
+                    throw new ArgumentNullException(("This field can not be null or empty!"));
+                }
+                else
+                {
+                FirstName = firstName;}
+
+                }
+
+        }
+        //}
+        //public string lastName
+        //{
+        //    get { return LastName; }
+        //    set {
+        //    LastName = value; }
+        //}
+    }
+
+}
--- Assignment_To-do It/Program.cs
+++ Assignment_To-do It/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace Assignment_To_do_It
+﻿using Assignment_To_do_It.Models;
+
+namespace Assignment_To_do_It
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+            Person p1 = new Person(1, "", "Godderi");
+            Person p2 = new Person(2, "Charles", "");
+            Person p3 = new Person(3, "Maria", "Svensson");
+            Console.WriteLine(p1.id + " " + p1.firstName);
+
         }
     }
 }
